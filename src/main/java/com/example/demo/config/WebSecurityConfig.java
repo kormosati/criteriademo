59a540fdf31ba0c5e50e8b3f2caaf7529afd9908
Andras.Podobni <andras.podobni.ext@evosoft.com>
2018-04-19 18:09:12 +0200

    create ldpa security2 branch for practice

--- src/main/java/com/example/demo/config/WebSecurityConfig.java
+++ src/main/java/com/example/demo/config/WebSecurityConfig.java
@@ -1,43 +1,36 @@
 package com.example.demo.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+import org.springframework.security.crypto.password.LdapShaPasswordEncoder;
 
 @Configuration
-@EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                .antMatchers("/", "/home").permitAll()
-                .anyRequest().authenticated()
+        http.authorizeRequests()
+                .anyRequest()
+                .fullyAuthenticated()
                 .and()
                 .formLogin()
-                .loginPage("/login")
-                .permitAll()
-                .and()
-                .logout()
-                .permitAll();
+                .defaultSuccessUrl("/index", true);
     }
 
-    @Bean
     @Override
-    public UserDetailsService userDetailsService() {
-        UserDetails user =
-                User.withDefaultPasswordEncoder()
-                        .username("podi")
-                        .password("0000")
-                        .roles("USER")
-                        .build();
-
-        return new InMemoryUserDetailsManager(user);
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.ldapAuthentication()
+                .userDnPatterns("uid={0},ou=people")
+                .groupSearchBase("ou=groups")
+                .contextSource()
+                    .url("ldap://localhost:8389/dc=springframework,dc=org")
+                    .and()
+                .passwordCompare()
+                    .passwordEncoder(PasswordEncoderFactories.createDelegatingPasswordEncoder())
+                .passwordAttribute("userPassword");
     }
 }
--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -3,32 +3,19 @@ package com.example.demo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by attila.kormos on 2018. 04. 19..
  */
 
-@Controller
+@RestController
 @RequestMapping(value = "/")
 public class HelloController {
 
-    @GetMapping("/hello")
-    public String hello () {
-        return "hello";
+    @GetMapping("/index")
+    public String index () {
+        return "Welcome to the home page!";
     }
 
-    @GetMapping("/home")
-    public String home () {
-        return "home";
-    }
-
-    @GetMapping("/login")
-    public String login () {
-        return "login";
-    }
-
-    @GetMapping("/logout")
-    public String logout () {
-        return "logout";
-    }
 }

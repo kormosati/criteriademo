a0c8bfc964fdc6234bda234e25dd8ab07892edfb
Andras.Podobni <andras.podobni.ext@evosoft.com>
2018-04-19 14:17:48 +0200

    create basic security

--- src/main/java/com/example/demo/config/WebSecurityConfig.java
+++ src/main/java/com/example/demo/config/WebSecurityConfig.java
@@ -33,8 +33,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     public UserDetailsService userDetailsService() {
         UserDetails user =
                 User.withDefaultPasswordEncoder()
-                        .username("user")
-                        .password("password")
+                        .username("podi")
+                        .password("0000")
                         .roles("USER")
                         .build();
 
